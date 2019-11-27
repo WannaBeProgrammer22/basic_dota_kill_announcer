fa03db808a76eb501a977b1d1cf7247d608fe87f
Jerwin Fernandez <jerwinfernandez04@gmail.com>
2019-11-27 17:49:37 +0800

    'Done'

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(DotaKillAnnouncer());
 
 class DotaKillAnnouncer extends StatelessWidget {
+  void playSound(String fileName) {
+    final AudioCache player = AudioCache();
+    player.play(fileName);
+  }
+
+  Expanded buildKey(String killName, String fileName, Color color) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(fileName);
+        },
+        child: Text(
+          killName,
+          style: TextStyle(
+            fontFamily: 'Creepster',
+            fontSize: 25.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            buildKey('Firstblood', 'firstblood.mp3', Colors.redAccent[400]),
+            buildKey('Double Kill', 'double_kill.mp3', Colors.blue[400]),
+            buildKey('Triple Kill', 'triple_kill.mp3', Colors.greenAccent[400]),
+            buildKey(
+                'Ultra Kill', 'ultra_kill.mp3', Colors.lightBlueAccent[100]),
+            buildKey('Rampage Kill', 'rampage.mp3', Colors.redAccent[700]),
+          ],
+        ),
+      ),
+    );
   }
 }
