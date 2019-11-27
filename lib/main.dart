b0fc4c4dfea60c0f984ff55f3a3c269f8da5a0b1
Jerwin Fernandez <jerwinfernandez04@gmail.com>
2019-11-27 17:24:19 +0800

    init commit with assets folder

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(DotaKillAnnouncer());
+
+class DotaKillAnnouncer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
